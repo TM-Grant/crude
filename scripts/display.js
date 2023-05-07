d28f77f2a6983913714fded0a5b17b9e4bc71496
TM Grant <109566073+TM-Grant@users.noreply.github.com>
2023-05-07 13:57:27 -0400

    Update display.js

--- scripts/display.js
+++ scripts/display.js
@@ -50,7 +50,7 @@ system.runInterval(() => {
 
 system.runInterval(() => {
     overworld.runCommandAsync("scoreboard players add animation Counters 1");
-    if (getScore("animation", "Counters") == 33)
+    if (getScore("animation", "Counters") == 29)
         overworld.runCommandAsync("scoreboard players set animation Counters 0");
 }, 4);
 
@@ -63,42 +63,3 @@ system.run(function tick() {
     }
     system.run(tick);
 });
-
-function sell_items(player) {
-    let sellable_items = {
-        "minecraft:ghast_tier": 25,
-        "minecraft:nether_star": 80,
-        "minecraft:fire_charge": 125,
-        "minecraft:lily_pad": 250,
-        "minecraft:ender_eye": 250,
-        "minecraft:coal": 17,
-        "minecraft:raw_iron": 30,
-        "minecraft:raw_gold": 50,
-        "minecraft:diamond": 100,
-        "minecraft:emerald": 150,
-        "minecraft:nether_quartz": 25
-    }
-}
-
-const inventory = player.getComponent("inventory").container,
-    { size } = inventory;
-const void_slot = undefined;
-let amount = 0;
-let item_count = 0;
-for (let i = 0; i < size; i++) {
-    const item = inventory.getItem(i);
-    if (!item) continue;
-    const item_price = sellable_items[item.typeId];
-    if (!item_price) continue;
-    amount += item_price * item.amount;
-    item_count += item.amount;
-    inventory.setItem(i, void_slot);
-}
-if (item_count < 1) {
-    player.sendMessage('§8[§eKitPvP§8] §cYou have no items to sell.§r');
-    return;
-}
-if (item_count >= 1) {
-    player.runCommandAsync(`scoreboard players add @s Balance ${amount}`);
-    player.sendMessage(`§8[§eKitPvP§8] §dYou sold §bx${item_count} §ditem(s) for §a$${amount}§d.§r`);
-}
