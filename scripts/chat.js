730fd29052552cb36fde04c10bec0557a93345dd
TM Grant <109566073+TM-Grant@users.noreply.github.com>
2023-05-07 13:58:46 -0400

    Update chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,7 +8,7 @@ world.events.beforeChat.subscribe((data) => {
     let hours = String(getScore(player, "Hours")).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     if (!message.startsWith(prefix)) {
         data.cancel = true;
-        world.sendMessage(`§7[§a$${balance}§7] §7[§b${hours}§7] §e${player.name}: §f${message}`)
+        world.sendMessage(`§7[§a$${balance}§7] §7[§b${hours}§7] §r§e${player.name}: §f${message}§r`)
     }
     else if (message.startsWith(prefix)) {
         data.cancel = true
