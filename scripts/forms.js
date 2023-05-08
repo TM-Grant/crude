68a8e883204c822d7f19da1eb1f137b21bd9bb52
TM Grant <109566073+TM-Grant@users.noreply.github.com>
2023-05-08 16:51:34 -0400

    Update forms.js

--- scripts/forms.js
+++ scripts/forms.js
@@ -15,11 +15,11 @@ function page1(player) {
     const form = new ActionFormData();
     form.title("§dCrude §8KitPvP§r");
     form.body("§eMade By: §fTM Grant§r");
-    form.button("§9Warps§r\n§7[§bClick To View§7]§r");
-    form.button("§dRules§r\n§7[§bClick To View§7]§r")
-    form.button("§bCodes§r\n§7[§bClick To View§7]§r")
-    form.button("§2Money Transfer§r\n§7[§bClick To Transfer§7]§r");
-    form.button("§cClose§r\n§7[§bClick To Close§7]§r");
+    form.button("§9Warps§r\n§7[§bClick To View]");
+    form.button("§dRules§r\n§7[§bClick To View]")
+    form.button("§bCodes§r\n§7[§bClick To View]")
+    form.button("§2Money Transfer§r\n§7[§bClick To Transfer]");
+    form.button("§cClose§r\n§7[§bClick To Close]");
     form.show(player).then((response) => {
         if (response.selection == 0) {
             page2(player)
@@ -69,20 +69,18 @@ function page2(player) {
 
 async function page3(player) {
     const allPlayers = world.getAllPlayers().map((plr) => plr.name);
-    const Balance = getScore(player, "money");
+    const Balance = getScore(player, "Money");
     const response = await new ModalFormData()
         .title("§2Money Transfer§r")
-        .body(`§l§dGui made by TM Grant`)
         .dropdown("Select a Player", allPlayers)
-        .textField(`Please insert the amount to transfer`, "Amount goes here...")
+        .textField("Please insert the amount to transfer", "Amount goes here...")
         .show(player).then((response) => {
             if (response.canceled) return;
             const target = world
                 .getAllPlayers()
                 .find((plr) => plr.name === allPlayers[response.formValues[0]]);
             if (target.name === player.name)
-                return player.sendMessage(
-                    "§8[§eKitPvP§8] §cYou can't send Balance to yourself. Why would you need to?§r"
+                return player.sendMessage("§8[§eKitPvP§8] §cYou can't send money to yourself. Why would you need to?§r"
                 );
             if (!target) return player.sendMessage("Player left the game!");
             const amount = parseInt(response.formValues[1]);
@@ -90,14 +88,10 @@ async function page3(player) {
                 return player.sendMessage("§8[§eKitPvP§8] §cInsufficient funds!§r");
             if (amount < 1)
                 return player.sendMessage("§8[§eKitPvP§8] §cNegative value.§r");
-            player.runCommandAsync(`scoreboard players remove @s money ${amount}`);
-            target.runCommandAsync(`scoreboard players add @s money ${amount}`);
-            player.sendMessage(
-                `§8[§eKitPvP§8] §fYou have sent §a$${amount} §fto §b${target.name}§r`
-            );
-            target.sendMessage(
-                `§8[§eKitPvP§8] §fYou have recieved §a$${amount} §ffrom §b${player.name}§r`
-            );
+            player.runCommandAsync(`scoreboard players remove @s Money ${amount}`);
+            target.runCommandAsync(`scoreboard players add @s Money ${amount}`);
+            player.sendMessage(`§8[§eKitPvP§8] §fYou have sent §a$${amount} §fto §b${target.name}§r`);
+            target.sendMessage(`§8[§eKitPvP§8] §fYou have recieved §a$${amount} §ffrom §b${player.name}§r`);
         });
 }
 
@@ -105,7 +99,7 @@ system.runInterval(() => {
     for (const player of world.getPlayers()) {
         if (player.hasTag("rules")) {
             player.sendMessage("§dRules \n§f - No Clogging\n - No Hacking/Exploiting\n - No Small/invis Skins\n - No Spamming/Flooding Chat\n - No Drag, Auto Clicking, jitter clicking\n - No ip pulling/Threats\n - No Abusing Glitches if you report a reward will be granted\n - Max Teams of two\n - Anyone Can kill loot stealers\n - You can only kill people your tier\n - No body blocking\n - No lying to staff\n - No disrespecting staff\n - No kit swapping\n - keep your cps under 15§r")
-            player.removeTag("rules")
+            player.removeTag("rules");
         }
     }
 })
@@ -114,7 +108,7 @@ system.runInterval(() => {
     for (const player of world.getPlayers()) {
         if (player.hasTag("codes")) {
             player.sendMessage("§cCodes§f\n - §dRealm Code: §fpFrsCHPkWTY\n - §9Discord: §fMRDjdeDDbY")
-            player.removeTag("codes")
+            player.removeTag("codes");
         }
     }
 })
